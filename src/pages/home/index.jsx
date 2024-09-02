46cc70612f701c45a75d2a989e00cdb7bc0692f9
Beginneraboutlife116 <pythonforeye@gmail.com>
2024-09-02 22:04:11 +0800

    Setting bootstrap file

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the Home page!</h1>
+      <h1 className="text-primary">Welcome to the Home page!</h1>
     </div>
   );
 }
